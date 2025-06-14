f1f47b0021225ea841064c04483aff9569732f6d
JOMEL DAVID P. POQUIZ <renzen.miracle@gmail.com>
2025-06-15 02:22:41 +0800

    MORE MUSIC (main menu)

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,6 +3,11 @@ extends Control
 signal play_pressed
 @onready var button_press: AudioStreamPlayer = $ButtonPress
 @onready var button_hover: AudioStreamPlayer = $ButtonHover
+@onready var main_menu_music: AudioStreamPlayer = $MainMenuMusic
+@onready var main_menu_music_2: AudioStreamPlayer = $MainMenuMusic2
+
+func _ready():
+	main_menu_music.play()
 
 func _on_start_pressed() -> void:
 	print("Main Menu: Starting game . . . ")
@@ -20,3 +25,11 @@ func _on_start_button_mouse_entered() -> void:
 
 func _on_quit_button_mouse_entered() -> void:
 	button_hover.play()
+
+
+func _on_main_menu_music_finished() -> void:
+	main_menu_music_2.play()
+
+
+func _on_main_menu_music_2_finished() -> void:
+	main_menu_music.play()
