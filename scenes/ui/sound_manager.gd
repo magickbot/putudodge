73126bc0fd19fd9df962af4b0fb1894ba743c8be
JOMEL DAVID P. POQUIZ <renzen.miracle@gmail.com>
2025-06-16 00:17:54 +0800

    added more music for more progression

--- scenes/ui/sound_manager.gd
+++ scenes/ui/sound_manager.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 @onready var bgm1 = $BGM1
 @onready var bgm2 = $BGM2
+@onready var bgm3 = $BGM3
+@onready var bgm4 = $BGM4
+
 @onready var button_hover: AudioStreamPlayer = $ButtonHover
 @onready var button_press: AudioStreamPlayer = $ButtonPress
 
@@ -13,8 +16,13 @@ func _on_bgm_1_finished() -> void:
 	bgm2.play()
 
 func _on_bgm_2_finished() -> void:
-	bgm1.play()
+	bgm3.play()
 
+func _on_bgm_3_finished() -> void:
+	bgm4.play()
+
+func _on_bgm_4_finished() -> void:
+	pass
 
 func _on_button_mouse_entered() -> void:
 	if not is_touchscreen_device():
